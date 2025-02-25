81b18d1e0f9dbd285e48531aeb4e1c405807b2ef
Ryan-AW <ryanawhittock@outlook.com>
2025-02-25 14:59:43 +0000

    created channel overview page (no graphic design); changed background color scheme.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 """ define all url routes """
 from flask import Blueprint, render_template
+from app.web_scraping_scripts import scrape_channel_videos
 
 
 bp = Blueprint('main', __name__)
@@ -8,3 +9,8 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/channel/<channel_id>')
+def channel_overview(channel_id):
+    return render_template('channel_overview.html', videos=scrape_channel_videos(channel_id))
