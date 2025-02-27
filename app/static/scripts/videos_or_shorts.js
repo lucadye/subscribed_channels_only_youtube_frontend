04d8b536428cea37f0bc773019ccbd85d075d346
Ryan-AW <ryanawhittock@outlook.com>
2025-02-27 20:19:11 +0000

    changed tab buttons into links

--- app/static/scripts/videos_or_shorts.js
+++ app/static/scripts/videos_or_shorts.js
@@ -1,19 +1,18 @@
 const container = document.getElementsByClassName('feed-container')[0];
 const selector = document.getElementsByClassName('feed-selector')[0];
-container.className = 'feed-container videos';
 
-selector.children[0].disabled = true;
-let currentTab = selector.children[0].className;
-for (let button of selector.children) {
-    button.onclick = e => {
-        e.preventDefault();
-        if (e.target.className !== currentTab) {
-            container.className = 'feed-container ' + e.target.className;
-            for (let btn of selector.children) {
-                btn.disabled = false;
-            }
-            e.target.disabled = true;
-            currentTab = e.target.className;
-        }
-    };
-}
+
+let currentTab = selector.querySelector('.active');
+
+selector.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.tagName === 'A' && e.target !== currentTab) {
+        container.className = 'feed-container ' + e.target.id;
+
+        currentTab.classList.remove('active');
+        e.target.classList.add('active');
+
+        currentTab = e.target;
+    }
+});
