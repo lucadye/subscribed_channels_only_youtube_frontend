ee4424a429fbbbded1cba7bd6fc081e13af05d2c
Ryan-AW <ryanawhittock@outlook.com>
2025-03-04 16:04:04 +0000

    implemented validation of video ids

--- app/web_scraping_scripts/scrape_video_page_data.py
+++ app/web_scraping_scripts/scrape_video_page_data.py
@@ -2,11 +2,15 @@
 
 import yt_dlp
 from app.datatypes import VideoType
+from app.validators import validate_video_id, ValidationError
 from app.web_scraping_scripts.data_conversion import human_readable_large_numbers, human_readable_times
 
 
 def scrape_video_data(video_id: str) -> VideoType:
     """ scrapes the metadata of a YouTube video """
+    if not validate_video_id(video_id):
+        raise ValidationError('invalid channel id')
+
     video_url = f'https://www.youtube.com/watch?v={video_id}'
 
     ydl_opts = {
