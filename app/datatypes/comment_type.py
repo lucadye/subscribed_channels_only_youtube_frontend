486da8e7eeb88e21386b8d335915a72e4743bb97
Ryan-AW <ryanawhittock@outlook.com>
2025-03-05 15:42:19 +0000

    implemented function to scrape all comments from a video

---
+++ app/datatypes/comment_type.py
@@ -0,0 +1,19 @@
+""" defines a dataclass to represent a viewer comment """
+from dataclasses import dataclass
+
+
+@dataclass
+class CommentType:
+    """ a dataclass to represent a viewer comment """
+    comment_id: str
+    text: str
+    like_count: str
+    author_id: str
+    author: str
+    author_thumbnail_url: str
+    author_is_uploader: bool
+    author_is_verified: bool
+    is_favorited: bool
+    is_pinned: bool
+    time_str: str
+    replies: list
