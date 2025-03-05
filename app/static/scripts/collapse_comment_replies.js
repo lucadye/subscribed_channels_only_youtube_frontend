f39d457c25a206ca5abadd285fcc45f45e9684ab
Ryan-AW <ryanawhittock@outlook.com>
2025-03-05 21:40:42 +0000

    implemented collapsing comment replies

---
+++ app/static/scripts/collapse_comment_replies.js
@@ -0,0 +1,27 @@
+var toggleButtons = document.querySelectorAll('.toggle-replies');
+
+function updateButtonText(button, repliesDiv) {
+    var replyCount = button.getAttribute('data-reply-count');
+    var showText = replyCount == 1 ? `show reply...` : `$show ${replyCount} replies...`;
+    var hideText = replyCount == 1 ? `hide reply...` : `$hide ${replyCount} replies...`;
+    button.textContent = repliesDiv.classList.contains('hidden') ? showText : hideText;
+}
+
+function toggleReplies(event) {
+    event.preventDefault();
+    var button = event.target;
+    var repliesDiv = button.parentNode.querySelector('.replies');
+
+    repliesDiv.classList.toggle('hidden');
+    updateButtonText(button, repliesDiv);
+}
+
+function initializeCollapsableReplies() {
+    toggleButtons.forEach(function (button) {
+        var repliesDiv = button.parentNode.querySelector('.replies');
+        updateButtonText(button, repliesDiv);
+        button.addEventListener('click', toggleReplies);
+    });
+}
+
+initializeCollapsableReplies();
