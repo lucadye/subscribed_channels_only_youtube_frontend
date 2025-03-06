46f4c2d114c3de6621f77218b5cfbeb48d5526e8
Ryan-AW <ryanawhittock@outlook.com>
2025-03-06 19:29:08 +0000

    added profile picture url to VideoType (changed import order to allow); changed quotes

--- app/web_scraping_scripts/scrape_video_page_data.py
+++ app/web_scraping_scripts/scrape_video_page_data.py
@@ -4,6 +4,7 @@ import yt_dlp
 from app.datatypes import VideoType
 from app.validators import validate_video_id, ValidationError
 from app.web_scraping_scripts.data_conversion import human_readable_large_numbers, human_readable_times
+from app.web_scraping_scripts import get_profile_icon
 
 
 def scrape_video_data(video_id: str) -> VideoType:
@@ -24,11 +25,12 @@ def scrape_video_data(video_id: str) -> VideoType:
 
     return VideoType(
         video_id=video_id,
-        channel_name=info_dict.get("channel", ""),
-        channel_id=info_dict.get("channel_id", ""),
-        title=info_dict.get("title", ""),
-        thumbnail=info_dict.get("thumbnail", ""),
-        views=human_readable_large_numbers(info_dict.get("view_count", None)),
-        description=info_dict.get("description", ""),
-        duration=human_readable_times(info_dict.get("duration", None))
+        channel_name=info_dict.get('channel', ''),
+        channel_id=info_dict.get('channel_id', ''),
+        title=info_dict.get('title', ''),
+        thumbnail=info_dict.get('thumbnail', ''),
+        views=human_readable_large_numbers(info_dict.get('view_count', None)),
+        description=info_dict.get('description', ''),
+        duration=human_readable_times(info_dict.get('duration', None)),
+        channel_pic=get_profile_icon(info_dict.get('channel_id', ''))
     )
