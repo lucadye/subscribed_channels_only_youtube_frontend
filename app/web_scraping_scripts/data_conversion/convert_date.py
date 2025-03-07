7066bfc40fd945d3459c0cb9d847905d423fded9
Ryan-AW <ryanawhittock@outlook.com>
2025-03-07 19:58:16 +0000

    changed function in order to convert youtube datestamps into a human format

---
+++ app/web_scraping_scripts/data_conversion/convert_date.py
@@ -0,0 +1,44 @@
+""" defines a function to convert a YouTube internal datestamp to a humanreadable date """
+def convert_date(youtube_date_stamp: str | None) -> str | None:
+    """ converts a YouTube internal datestamp to a humanreadable date """
+
+    # Account for epoch being None
+    if youtube_date_stamp is None:
+        return None
+
+    MONTH_MAP = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    # maps number suffixes like "th" or "rd" using unicode
+    SUFFIX_MAP = {
+        '1': '\u02E2\u1D57',
+        '2': '\u207F\u1D48',
+        '3': '\u02B3\u1D48',
+        '4': '\u1D57\u02B0',
+        '5': '\u1D57\u02B0',
+        '6': '\u1D57\u02B0',
+        '7': '\u1D57\u02B0',
+        '8': '\u1D57\u02B0',
+        '9': '\u1D57\u02B0',
+        '0': '\u1D57\u02B0'
+    }
+
+    year = youtube_date_stamp[:4]
+    month_number = int(youtube_date_stamp[4:6]) - 1
+    day = int(youtube_date_stamp[6:]) - 1
+
+    suffix = SUFFIX_MAP[str(day)[-1]]
+    month = MONTH_MAP[month_number-1]
+    return f"{day}{suffix} of {month} {year}"
