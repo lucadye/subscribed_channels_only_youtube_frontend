d3fd7f81394d1dc6926b0c956fd4d04394c5d448
Ryan-AW <ryanawhittock@outlook.com>
2025-03-12 21:50:14 +0000

    made channel info optional for videos on the channel's page

--- app/datatypes/video_preview.py
+++ app/datatypes/video_preview.py
@@ -6,12 +6,12 @@ from dataclasses import dataclass
 @dataclass
 class VideoPreviewType(VideoBaseType):
     """ a dataclass that stores metadata about a YouTube video preview """
-    channel_id: str
-    channel_name: str
     title: str
     views: str
     thumbnail: str
     description: str
     duration: str
-    channel_pic: str
     date_stamp: str
+    channel_id: str = None
+    channel_name: str = None
+    channel_pic: str = None
