6113165c0b0d43a3647751b827ef934448ef9520
Ryan-AW <ryanawhittock@outlook.com>
2025-03-19 00:51:49 +0000

    created search page animation handling class

--- app/static/scripts/animated_background.js
+++ app/static/scripts/animated_background.js
@@ -1,86 +1,11 @@
-let secret_mode = false;
-
-function onKonamiCode(cb) {
-    var input = '';
-    var key = '38384040373937396665';
-    document.addEventListener('keydown', function (e) {
-      input += ("" + e.keyCode);
-      if (input === key) {
-        return cb();
-      }
-      if (!key.indexOf(input)) return;
-      input = ("" + e.keyCode);
-    });
-}
-  
-onKonamiCode(function () {
-    secret_mode = true;
-    color_index = 0;
-    colors = secret_colors;
-})
-
-
-
+// setup canvas
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
+
+// implement canvas resizing
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-const default_speed = 0.2; // In seconds
-
-const default_colors = [
-    '#007F00',
-    '#00FF00',
-    '#FFFF00',
-    '#FF7F00',
-    '#FF0000',
-    '#7F007F',
-    '#FF00FF',
-    '#7F00FF',
-    '#0000FF',
-    '#007FFF',
-    '#00FFFF'
-];
-
-const secret_speed = 0.02; // In seconds
-
-const secret_colors = [
-    '#5BCEFA',
-    '#F5A9B8',
-    '#FFFFFF',
-    '#F5A9B8'
-]
-
-var frame_num = 0;
-var color_index = 0
-var colors = default_colors;
-
-
-function animate() {
-    frame_num = frame_num + 1;
-    if (frame_num % 60 == 0) {
-        color_index = color_index + 1;
-        if (color_index >= colors.length) {
-            color_index = 0;
-        }
-    }
-
-    c.fillStyle = 'rgba(0, 0, 0, 0.01)';
-    c.fillRect(0, 0, canvas.width, canvas.height);
-
-    c.moveTo(0, canvas.height/2);
-    c.beginPath();
-    for (let i=0; i<canvas.width; i++){
-        c.lineTo(i, (canvas.height/2) + (Math.sin((i+frame_num) * 0.01) * ((canvas.height-50) / 2)));
-    }
-    c.strokeStyle = colors[color_index];
-    c.stroke();
-
-    setTimeout(function(){
-        animate(colors);
-    }, 1000 * (secret_mode ? secret_speed : default_speed));
-}
 
 window.addEventListener('resize', function(event) {
     canvas.width = window.innerWidth;
@@ -88,4 +13,82 @@ window.addEventListener('resize', function(event) {
 }, true);
 
 
-animate();
+// create class to handle executing animations
+class Animator {
+    constructor(animationSpeed = 1000) {
+        this._animationSpeed = animationSpeed;
+        this._frameNum = 0;
+        this._currentAnimation = null;
+        this._isAnimating = false;
+    }
+
+    get animationSpeed() {
+        return this._animationSpeed;
+    }
+
+    set animationSpeed(value) {
+        if (value >= 0) {
+            this._animationSpeed = value;
+        } else {
+            console.error("`animationSpeed` must be a greater than 0.");
+        }
+    }
+
+    get currentAnimation() {
+        return this._currentAnimation;
+    }
+
+    set currentAnimation(animation) {
+        this._currentAnimation = animation;
+    }
+
+    get isAnimating() {
+        return this._isAnimating;
+    }
+
+    _animate() {
+        // stop animating if no animation function is provided
+        if (this._currentAnimation == null) {
+            this._isAnimating = false;
+            return;
+        }
+
+	// animate frame
+        this._currentAnimation(this._frameNum);
+        this._frameNum += 1;
+
+	// recurse to animate next frame
+        this._isAnimating = true;
+        setTimeout(() => {
+            this._animate();
+        }, this._animationSpeed);
+    }
+
+    startAnimation(animation) {
+	// tell the animation to stop
+	this.stopAnimation();
+
+        // wait for the last frame of the animation to be executed
+        if (this._isAnimating) {
+            setTimeout(() => {
+                this.startAnimation(animation);
+            }, 50);
+        }
+	
+	// start the new animation
+	else {
+            this.currentAnimation = animation;
+            this._frameNum = 0;
+            this._animate();
+        }
+    }
+
+    stopAnimation() {
+        this.currentAnimation = null;
+    }
+}
+
+
+// use the animator instance to handle running animations
+const animator = new Animator(1000);
+
