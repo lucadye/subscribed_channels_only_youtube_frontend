fdab9149b1ee84303ee10a48dc977aef66d930a4
Ryan-AW <ryanawhittock@outlook.com>
2025-03-19 01:25:44 +0000

    added sine wave animation functions

--- app/static/scripts/animated_background.js
+++ app/static/scripts/animated_background.js
@@ -15,7 +15,7 @@ window.addEventListener('resize', function(event) {
 
 // create class to handle executing animations
 class Animator {
-    constructor(animationSpeed = 1000) {
+    constructor(animationSpeed = 200) {
         this._animationSpeed = animationSpeed;
         this._frameNum = 0;
         this._currentAnimation = null;
@@ -90,5 +90,71 @@ class Animator {
 
 
 // use the animator instance to handle running animations
-const animator = new Animator(1000);
+const animator = new Animator();
 
+
+/*
++=====================+
++ animation templates +
++=====================+
+*/
+
+
+function colorSineWaveTemplate(frameNum, colorArray, framesPerColor) {
+    const _index = Math.floor(frameNum / 7);
+
+    c.fillStyle = 'rgba(0, 0, 0, 0.01)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    c.moveTo(0, canvas.height/2);
+    c.beginPath();
+    for (let i=0; i<canvas.width; i++){
+        c.lineTo(i, (canvas.height/2) + (Math.sin((i+frameNum) * 0.01) * ((canvas.height-50) / 2)));
+    }
+    c.strokeStyle = colorArray[_index % colorArray.length];
+    c.stroke();
+}
+
+
+/*
++=====================+
++ animation functions +
++=====================+
+*/
+
+function rainbowSineWaveAnimation(frameNum) {
+    animator.animationSpeed = 200;
+    const framesPerColor = 7;
+
+    let colorArray = [
+        '#007F00',
+        '#00FF00',
+        '#FFFF00',
+        '#FF7F00',
+        '#FF0000',
+        '#7F007F',
+        '#FF00FF',
+        '#7F00FF',
+        '#0000FF',
+        '#007FFF',
+        '#00FFFF'
+    ];
+
+    colorSineWaveTemplate(frameNum, colorArray, framesPerColor)
+}
+
+function transFlagSineWaveAnimation(frameNum) {
+    animator.animationSpeed = 20;
+    const framesPerColor = 40;
+
+    let colorArray = [
+        '#5BCEFA',
+        '#F5A9B8',
+        '#FFFFFF',
+        '#F5A9B8',
+        '#5BCEFA',
+	'#000000'
+    ];
+
+    colorSineWaveTemplate(frameNum, colorArray, framesPerColor)
+}
