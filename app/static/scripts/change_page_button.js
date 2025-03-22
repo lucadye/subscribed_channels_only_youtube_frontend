0b5c80236643a5491e60460a7a3081d2e4763256
Ryan-AW <ryanawhittock@outlook.com>
2025-03-22 23:43:28 +0000

    implemented switching between pages

--- app/static/scripts/change_page_button.js
+++ app/static/scripts/change_page_button.js
@@ -1,3 +1,22 @@
+let currentPageNum = 1
+
+
+function updateCurrentPageNum() {
+    const pageNumElement = document.getElementById("currentPageNum");
+    pageNumElement.textContent = currentPageNum;
+
+    const videoFeed = document.getElementById("video-feed");
+    let index = 1
+    for (const page of videoFeed.children) {
+        page.classList.add("hidden");
+        if (index == currentPageNum) {
+    	    page.classList.remove("hidden");
+        }
+	index ++;
+    }
+}
+
+
 function createVideoElement(videoData) {
     const videoContainer = document.createElement('div');
     videoContainer.className = 'video-container';
@@ -20,7 +39,7 @@ function createVideoElement(videoData) {
 
     // video info
     const viewCount = document.createElement('p');
-    viewCount.textContent = `${videoData.views} views`;
+    viewCount.textContent = videoData.views;
 
     const titleLink = document.createElement('a');
     titleLink.href = `/video/${videoData.video_id}`;
@@ -53,8 +72,8 @@ function createPageElement(pageData) {
 
 
 
-let nextPageToken = null;
 let hasMorePages = true;
+let numPagesLoaded = 1;
 
 
 function fetchNextChannelPage(playlistId, nextPageToken) {
@@ -68,16 +87,39 @@ function fetchNextChannelPage(playlistId, nextPageToken) {
     .then(response => response.json());
 }
 
-document.getElementById('nextPageButton').addEventListener('click', function() {
-    if (hasMorePages) {
+
+
+
+
+function moveToNextPage() {
+    if (currentPageNum < numPagesLoaded) {
+	currentPageNum ++;
+	updateCurrentPageNum();
+    }
+    else if (currentPageNum == numPagesLoaded && hasMorePages) {
         fetchNextChannelPage(playlist_id, nextPageToken)
             .then(data => {
                 createPageElement(data.page);
+
+		numPagesLoaded ++;
+		currentPageNum = numPagesLoaded;
+		updateCurrentPageNum();
+
                 nextPageToken = data['next-page-token'];
                 if (nextPageToken == null) {
             	hasMorePages = false;
                 }
             });
     }
-});
+}
 
+function moveToPreviousPage() {
+    if (currentPageNum > 1) {
+	currentPageNum --;
+	    updateCurrentPageNum();
+    }
+}
+
+
+document.getElementById('nextPageButton').addEventListener('click', moveToNextPage);
+document.getElementById('previousPageButton').addEventListener('click', moveToPreviousPage);
