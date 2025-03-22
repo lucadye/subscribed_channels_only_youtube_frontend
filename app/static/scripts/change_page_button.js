90accac7eba4712f2a28c2036f3778aa6d1913cd
Ryan-AW <ryanawhittock@outlook.com>
2025-03-22 21:28:21 +0000

    added function to create new page element

--- app/static/scripts/change_page_button.js
+++ app/static/scripts/change_page_button.js
@@ -1,3 +1,58 @@
+function createVideoElement(videoData) {
+    const videoContainer = document.createElement('div');
+    videoContainer.className = 'video-container';
+
+    // create thumbnail container and it's contents
+    const thumbnailContainer = document.createElement('a');
+    thumbnailContainer.className = 'thumbnail-container';
+    thumbnailContainer.href = `/video/${videoData.video_id}`;
+
+    const thumbnailImg = document.createElement('img');
+    thumbnailImg.src = videoData.thumbnail;
+    thumbnailImg.loading = 'lazy';
+
+    const durationOverlay = document.createElement('span');
+    durationOverlay.textContent = videoData.duration;
+
+    thumbnailContainer.appendChild(thumbnailImg);
+    thumbnailContainer.appendChild(durationOverlay);
+
+
+    // video info
+    const viewCount = document.createElement('p');
+    viewCount.textContent = `${videoData.views} views`;
+
+    const titleLink = document.createElement('a');
+    titleLink.href = `/video/${videoData.video_id}`;
+
+    const titleContents = document.createElement('h2');
+    titleContents.textContent = videoData.title;
+    titleLink.appendChild(titleContents);
+
+    // append the contents to the video container element
+    videoContainer.appendChild(thumbnailContainer);
+    videoContainer.appendChild(viewCount);
+    videoContainer.appendChild(titleLink);
+
+    return videoContainer;
+}
+
+function createPageElement(pageData) {
+    const page = document.createElement("div");
+    page.className = "page";
+
+    pageData.forEach(videoData => {
+        const videoElement = createVideoElement(videoData);
+        page.appendChild(videoElement);
+    });
+
+    const parentElement = document.getElementById('video-feed');
+    parentElement.appendChild(page);
+}
+
+
+
+
 let next_page_token = null;
 
 
@@ -11,12 +66,6 @@ function fetchNextChannelPage(playlistId, nextPageToken) {
     })
     .then(response => response.json());
 }
-
-function createPageElement(pageData) {
-    // add logic to create a new page
-    console.log(pageData);
-}
-
 
 document.getElementById('nextPageButton').addEventListener('click', function() {
     fetchNextChannelPage(playlist_id, next_page_token)
