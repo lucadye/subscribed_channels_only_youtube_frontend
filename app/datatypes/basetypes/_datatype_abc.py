34cb8cde8fe499f0c1eff56d30e74192097c3f27
Ryan-AW <ryanawhittock@outlook.com>
2025-03-30 20:46:13 +0100

    created validation function for dataclass input arguments

--- app/datatypes/basetypes/_datatype_abc.py
+++ app/datatypes/basetypes/_datatype_abc.py
@@ -1,11 +1,12 @@
 """ implements a base class for all data classes """
 from abc import ABC
-from inspect import stack
+from inspect import stack, currentframe
 
 
 class DatatypeABC(ABC):
     """ implements a base class for all data classes """
-    def _validate_setter_type(self, value, valid_types: list|set|tuple):
+    def _validate_setter_type(self, value, valid_types: list|set|tuple, attribute_name: str=None):
+        """ allows type validation for setter input values. """
         def raise_error(attribute_name: str, value, valid_types):
             type_names = [t.__name__ if t is not None else 'None' for t in valid_types]
 
@@ -34,7 +35,22 @@ class DatatypeABC(ABC):
             return
 
         if not any(isinstance(value, _type) for _type in valid_types if _type is not None):
+            if attribute_name is not None:
+                raise_error(str(attribute_name), value, valid_types)
             raise_error(f'{self.__class__.__name__}.{stack()[1].function}', value, valid_types)
+
+    def _validate_argument_type(self, variable, valid_types: list|set|tuple):
+        """ allows type validation for function input arguments """
+        for variable_name, variable_val in currentframe().f_back.f_locals.items():
+            if variable_val is variable:
+                self._validate_setter_type(
+                    value=variable,
+                    valid_types=valid_types,
+                    attribute_name=variable_name
+                )
+                break
+        else:
+            raise ValueError('Variable name can not be found.')
 
     def _serialize_data(self) -> dict:
         properties = {}
@@ -48,4 +64,3 @@ class DatatypeABC(ABC):
         properties = self._serialize_data()
         properties_str = ', '.join(f'{key}={value!r}' for key, value in properties.items())
         return f'{self.__class__.__name__}({properties_str})'
-
