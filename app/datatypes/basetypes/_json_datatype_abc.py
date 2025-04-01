dd30cd98284c78149ec076e37fa517a8f10db82e
Ryan-AW <ryanawhittock@outlook.com>
2025-04-01 17:48:43 +0100

    created datatype that can serialize to json

--- app/datatypes/basetypes/_json_datatype_abc.py
+++ app/datatypes/basetypes/_json_datatype_abc.py
@@ -1,6 +1,15 @@
 """ implements a base datatype that can be converted to json """
+from typing import Callable, TypeVar
+
 from json import dumps
 from ._datatype_abc import DatatypeABC
+
+
+F = TypeVar('F', bound=Callable)
+
+def json_include(func: F) -> F:
+    """ defines a decorator that addes a property to the json output """
+    return func
 
 
 class JsonDatatypeABC(DatatypeABC):
@@ -10,4 +19,4 @@ class JsonDatatypeABC(DatatypeABC):
         return dumps(self._json_compatible_serialize_data(), indent=4)
 
     def _json_compatible_serialize_data(self) -> dict:
-        return self._serialize_data()
+        return self._serialize_data(json_include)
