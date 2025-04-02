d8ccd3557f93c95549f7117fcf2c58acb570b6f4
Ryan-AW <ryanawhittock@outlook.com>
2025-04-02 20:18:23 +0100

    moved decorators into the sub-module's namespace

--- app/datatypes/basetypes/__init__.py
+++ app/datatypes/basetypes/__init__.py
@@ -1,2 +1,2 @@
-from ._datatype_abc import DatatypeABC
-from ._json_datatype_abc import JsonDatatypeABC
+from ._datatype_abc import DatatypeABC, constructor_include
+from ._json_datatype_abc import JsonDatatypeABC, json_include
