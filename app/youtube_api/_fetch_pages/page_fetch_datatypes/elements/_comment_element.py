cef2fddfc4d28c58834fa2703acf23e3395948f3
Ryan-AW <ryanawhittock@outlook.com>
2025-04-05 19:25:16 +0100

    changed constructor; fixed replies array

--- app/youtube_api/_fetch_pages/page_fetch_datatypes/elements/_comment_element.py
+++ app/youtube_api/_fetch_pages/page_fetch_datatypes/elements/_comment_element.py
@@ -1,53 +1,63 @@
 """ defines a dataclass for representing a youtube comment (and allowing json serialization) """
 from __future__ import annotations
 from typing import List
-from dataclasses import dataclass, field
 
 from flask import url_for
 
-from app.datatypes.basetypes import JsonDatatypeABC, json_include
+from app.datatypes.basetypes import JsonDatatypeABC, constructor_include, json_include
 from app.youtube_api.youtube_data_convertions import human_readable_large_numbers, \
         human_readable_time_delta
 
 
-@dataclass
 class JsonCommentElement(JsonDatatypeABC):
     """ defines a dataclass for representing a youtube comment (and allowing json serialization) """
-    _comment_id: str
-    _text: str
-    _like_count: int
-    _author: str
-    _author_id: str
-    _author_thumbnail_url: str
-    _is_author_uploader: bool
-    _time_stamp: str
-    _replies: List[JsonCommentElement] = field(default_factory=list)
+    def __init__(self,
+            comment_id: str,
+            text: str,
+            like_count: int,
+            author: str,
+            author_id: str,
+            author_thumbnail_url: str,
+            is_author_uploader: bool,
+            time_stamp: str,
+            replies: List[JsonCommentElement] = None):
 
-    def __post_init__(self):
-        self.validate_setter_type(self._comment_id, {str}, 'comment_id')
-        self.validate_setter_type(self._text, {str}, 'text')
-        self.validate_setter_type(self._like_count, {int}, 'like_count')
-        self.validate_setter_type(self._author, {str}, 'author')
-        self.validate_setter_type(self._author_id, {str}, 'author_id')
-        self.validate_setter_type(self._author_thumbnail_url, {str}, 'author_thumbnail_url')
-        self.validate_setter_type(self._is_author_uploader, {bool}, 'is_author_uploader')
-        self.validate_setter_type(self._time_stamp, {str}, 'time_stamp')
+        self.validate_setter_type(comment_id, {str}, 'comment_id')
+        self.validate_setter_type(text, {str}, 'text')
+        self.validate_setter_type(like_count, {int}, 'like_count')
+        self.validate_setter_type(author, {str}, 'author')
+        self.validate_setter_type(author_id, {str}, 'author_id')
+        self.validate_setter_type(author_thumbnail_url, {str}, 'author_thumbnail_url')
+        self.validate_setter_type(is_author_uploader, {bool}, 'is_author_uploader')
+        self.validate_setter_type(time_stamp, {str}, 'time_stamp')
 
-        self._reply_array = []
+        self._comment_id = comment_id
+        self._text = text
+        self._like_count = like_count
+        self._author = author
+        self._author_id = author_id
+        self._author_thumbnail_url = author_thumbnail_url
+        self._is_author_uploader = is_author_uploader
+        self._time_stamp = time_stamp
+
+        self._replies = [] if replies is None else replies
         self.append_reply(*self._replies)
 
     @property
     @json_include
+    @constructor_include
     def comment_id(self) -> str:
         """ returns the youtube id of the comment """
         return self._comment_id
 
     @property
+    @constructor_include
     def text(self) -> str:
         """ returns the text content of the comment """
         return self._text
 
     @property
+    @constructor_include
     def like_count(self) -> int:
         """ returns the raw number of likes """
         return self._like_count
@@ -62,11 +72,13 @@ class JsonCommentElement(JsonDatatypeABC):
 
     @property
     @json_include
+    @constructor_include
     def author(self) -> str:
         """ returns the account name of the comment's author """
         return self._author
 
     @property
+    @constructor_include
     def author_id(self) -> str:
         """ returns the account id of the comment's author """
         return self._author_id
@@ -85,17 +97,20 @@ class JsonCommentElement(JsonDatatypeABC):
 
     @property
     @json_include
+    @constructor_include
     def author_thumbnail_url(self) -> str:
         """ returns the profile picture url for the comment's author """
         return self._author_thumbnail_url
 
     @property
     @json_include
+    @constructor_include
     def is_author_uploader(self) -> bool:
         """ True if the uploader of the video the comment was posted on is the comment's author """
         return self._is_author_uploader
 
     @property
+    @constructor_include
     def time_stamp(self) -> str:
         """ returns youtube's timestamp of when the comment was posted """
         return self._time_stamp
@@ -108,14 +123,15 @@ class JsonCommentElement(JsonDatatypeABC):
 
     @property
     @json_include
+    @constructor_include
     def replies(self) -> List[JsonCommentElement]:
         """ returns the comment's replies as an array """
-        return self._reply_array
+        return self._replies
 
     @property
     def reply_count(self) -> int:
         """ returns the raw number of replies """
-        return len(self._reply_array)
+        return len(self._replies)
 
     @property
     @json_include
@@ -129,7 +145,7 @@ class JsonCommentElement(JsonDatatypeABC):
         """ adds more replies """
         for reply in replies:
             self.validate_argument_type(reply, {JsonCommentElement})
-        self._reply_array.extend(replies)
+        self._replies.extend(replies)
 
     def json_compatible_serialize_data(self) -> dict:
         """ returns the data as a dictionary """
