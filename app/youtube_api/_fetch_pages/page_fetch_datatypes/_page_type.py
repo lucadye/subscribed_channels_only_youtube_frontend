21e54599116304b271eb4e4a1c86464c75794461
Ryan-AW <ryanawhittock@outlook.com>
2025-04-08 21:49:23 +0100

    fixed bad type hinting

--- app/youtube_api/_fetch_pages/page_fetch_datatypes/_page_type.py
+++ app/youtube_api/_fetch_pages/page_fetch_datatypes/_page_type.py
@@ -5,7 +5,7 @@ from ._api_page_token import ApiPageToken
 
 class PageType(JsonDatatypeABC):
     """ json-serializable page of content for GET requests """
-    def __init__(self, page: JsonDatatypeABC, page_token: ApiPageToken):
+    def __init__(self, page, page_token: ApiPageToken):
         self.validate_argument_type(page_token, {ApiPageToken})
 
         self._page = page
@@ -21,6 +21,6 @@ class PageType(JsonDatatypeABC):
     @property
     @json_include
     @constructor_include
-    def page_token(self) -> str:
+    def page_token(self) -> ApiPageToken:
         """ returns the token datatype that is needed to fetch the next page """
         return self._page_token
