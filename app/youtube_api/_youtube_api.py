5fb554595d83d2a0d1f047c3b875e363aadaade2
Ryan-AW <ryanawhittock@outlook.com>
2025-04-20 18:26:52 +0100

    added docstrings to _youtube_api.py

--- app/youtube_api/_youtube_api.py
+++ app/youtube_api/_youtube_api.py
@@ -1,3 +1,4 @@
+""" implements a class for handling high level api requests """
 from typing import List
 from app.datatypes import VideoType, ChannelType
 
@@ -9,22 +10,28 @@ from . import misc_fetch_functions as _misc_fetch_functions
 
 
 class YouTubeAPI:
+    """ handles high level api requests """
     def __init__(self):
         self._api = YoutubeDataV3API(api_key=APIKey.VALUE)
         self._get_requests_handler = GetRequestsHandler(self._api)
 
     @property
     def get_requests(self):
+        """ returns handler for handling GET requested api queries """
         return self._get_requests_handler
 
     def fetch_video_info(self, video_id: str) -> VideoType:
+        """ returns info about a video """
         return _misc_fetch_functions.fetch_video_info(self._api, video_id)
 
     def fetch_channel_info(self, channel_id: str) -> ChannelType:
+        """ returns info about a channel """
         return _misc_fetch_functions.fetch_channel_info(self._api, channel_id)
 
     def fetch_profile_pictures(self, *channel_ids: [str]) -> List[str]:
+        """ retrieves the urls of the profile pictures for multiple channel in parallel """
         return _misc_fetch_functions.fetch_profile_pictures(self._api, *channel_ids)
 
     def fetch_profile_picture(self, channel_id: str) -> str:
+        """ retrieves the url of the profile picture for an individual channel  """
         return _misc_fetch_functions.fetch_profile_picture(self._api, channel_id)
